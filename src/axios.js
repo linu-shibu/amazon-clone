1bfb854518e42abe25c80ef9e423de84e195cff5
linu-shibu <72352875+linu-shibu@users.noreply.github.com>
2021-12-02 01:06:30 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // API URL
-  baseURL: "http://localhost:5001/clone-ec729/us-central1/api",
+  baseURL: "",
 });
 
 export default instance;
